c09fb5ab9b1b83ae8f38653a0f682b237ca3e9a5
Ashrafulhiderjayed <ashrafulhider87@gmail.com>
2024-09-19 13:56:28 +0600

    user delete api

--- index.js
+++ index.js
@@ -38,6 +38,7 @@ async function run() {
       const result = await userCollection.find().toArray();
       res.send(result);
     })
+
     app.post('/users', async(req, res) =>{
       const user = req.body;
       const query = { email: user.email };
@@ -51,6 +52,14 @@ async function run() {
       res.send(result);
     })
 
+    app.delete('/users/:id', async(req, res) =>{
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await userCollection.deleteOne(query);
+      res.send(result);
+    })
+
+    //menu related api ===============================
     app.get('/menu', async (req, res) => {
       const result = await menuCollection.find().toArray();
       res.send(result);
